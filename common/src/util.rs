0d0eaa77f995b49c088846202e75d40661c8b704
Aaron Fabbri <ajfabbri@gmail.com>
2023-11-01 14:53:14 -0700

    Add code for bootstrapping and waiting for peers

--- common/src/util.rs
+++ common/src/util.rs
@@ -1,1 +1,8 @@
+use std::time;
 
+pub fn system_time_msec() -> u64 {
+    time::SystemTime::now()
+        .duration_since(time::UNIX_EPOCH)
+        .expect("SystemTime::now")
+        .as_millis().try_into().unwrap()
+}
