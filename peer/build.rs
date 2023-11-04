9461733f078a47b4c6f17438eec783b988062170
Aaron Fabbri <ajfabbri@gmail.com>
2023-11-03 17:27:44 -0700

    cargo: Flesh out build logic

---
+++ peer/build.rs
@@ -0,0 +1,8 @@
+// Add /lib/gcc/x86_64-linux-gnu/12/ to linker library search path
+
+
+fn main() {
+    // Experimenting with static linking...
+    //println!("cargo:rustc-link-search=/lib/gcc/x86_64-linux-gnu/12/");
+    //println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu/");
+}
