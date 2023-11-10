7a3cd74f86a322e4ed05874406c550d820e72e8f
Aaron Fabbri <ajfabbri@gmail.com>
2023-11-09 19:37:32 -0800

    Refine document definitions, flesh out logic.
    - Define HeartbeatsDoc as a vector of peer heartbeat records.
    - Stash collections and doc ids in peer/coord context.
    - Add peer/presence monitoring and other debugging.
    - Use constants for collection names
    - coordinator: use offlineplayground and fix init issues.
    - Name containers and set ditto device_name accordingly.
    - docker: `expose` ports only at container network level

--- common/src/default.rs
+++ common/src/default.rs
@@ -2,10 +2,14 @@ use std::default::Default;
 use crate::types::{ExecutionPlan, CoordinatorInfo};
 use crate::util;
 
+pub const HEARTBEAT_COLLECTION_NAME: &str = "cmesh-heartbeat";
+pub const REPORT_COLLECTION_NAME: &str = "cmesh-report";
+pub const PEER_COLLECTION_NAME: &str = "cmesh-peers";
+
 impl Default for CoordinatorInfo {
     fn default() -> Self {
         CoordinatorInfo {
-            heartbeat_collection_name: "cmesh-heartbeat".to_string(),
+            heartbeat_collection_name: HEARTBEAT_COLLECTION_NAME.to_string(),
             heartbeat_interval_sec: 5,
             execution_plan: None,
         }
@@ -17,8 +21,8 @@ impl Default for ExecutionPlan {
         ExecutionPlan {
             start_time: util::system_time_msec() + 5000,
             test_duration_sec: 10,
-            report_collection_name: "cmesh-report".to_string(),
-            peer_collection_name: "cmesh-peers".to_string(),
+            report_collection_name: REPORT_COLLECTION_NAME.to_string(),
+            peer_collection_name: PEER_COLLECTION_NAME.to_string(),
             min_msg_delay_msec: 100,
             max_msg_delay_msec: 1000,
             peers: Vec::new(),
--- common/src/util.rs
+++ common/src/util.rs
@@ -1,8 +1,15 @@
 use std::time;
+use crate::types::Heartbeat;
 
 pub fn system_time_msec() -> u64 {
     time::SystemTime::now()
         .duration_since(time::UNIX_EPOCH)
         .expect("SystemTime::now")
         .as_millis().try_into().unwrap()
+}
+
+impl Heartbeat {
+    pub fn update_timestamp(&mut self) {
+        self.sent_at_msec = system_time_msec();
+    }
 }
