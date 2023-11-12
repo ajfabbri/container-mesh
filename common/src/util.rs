4555e7641a50eca683eecc223d46b9d6c2e63678
Aaron Fabbri <ajfabbri@gmail.com>
2023-11-11 17:55:21 -0800

    net: print local interface addrs. on init

--- common/src/util.rs
+++ common/src/util.rs
@@ -1,8 +1,9 @@
 use crate::types::Heartbeat;
 use dittolive_ditto::transport::{Peer, PresenceGraph};
-use std::time;
-use std::io;
+use local_ip_address::{list_afinet_netifas, local_ip};
 use serde_json;
+use std::io;
+use std::time;
 
 pub fn system_time_msec() -> u64 {
     time::SystemTime::now()
@@ -47,4 +48,13 @@ pub fn print_cdoc(cbor: &serde_cbor::Value) -> Result<(), io::Error> {
     serde_json::to_writer_pretty(std::io::stdout(), cbor)?;
     println!();
     Ok(())
+}
+
+pub fn show_local_ips() {
+    println!("==> Local IP address: {}", local_ip().unwrap());
+    let network_interfaces = list_afinet_netifas().unwrap();
+
+    for (name, ip) in network_interfaces.iter() {
+        println!("{}:\t{:?}", name, ip);
+    }
 }
