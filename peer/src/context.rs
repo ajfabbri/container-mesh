fb1e1f0839770ca4624fa78dd674ca9ff40e8f63
Aaron Fabbri <ajfabbri@gmail.com>
2023-11-21 15:32:44 -0800

    peer: extract context to separate file

---
+++ peer/src/context.rs
@@ -0,0 +1,60 @@
+use std::{thread::JoinHandle, sync::{Arc, Mutex}, error::Error};
+
+use common::{types::*, util::system_time_msec};
+use dittolive_ditto::prelude::*;
+
+use crate::HeartbeatCtx;
+
+
+pub struct PeerContext {
+    pub id: PeerId,
+    pub ditto: Ditto,
+    pub coord_addr: Option<String>,
+    pub coord_doc_id: Option<DocumentId>,
+    pub coord_info: Option<CoordinatorInfo>,
+    // Keep a copy of our last transport config so we can modify and re-set it.
+    pub transport_config: Option<TransportConfig>,
+    #[allow(dead_code)]
+    pub hb_doc_id: Option<DocumentId>,
+    pub hb_ctx: Option<HeartbeatCtx>,
+    pub hb_thread: Option<JoinHandle<Result<(), std::io::Error>>>,
+    #[allow(dead_code)]
+    pub start_time_msec: u64,
+    pub local_ip: String,
+    pub state: Arc<Mutex<PeerState>>,
+    pub peer_collection: Option<Arc<Mutex<Collection>>>,
+    pub peer_observer: Option<LiveQuery>,
+}
+
+impl PeerContext {
+    pub fn new(device_name: &str, ditto: Ditto, local_ip: &str) -> Self {
+        Self {
+            id: random_peer_id(Some(&device_name)),
+            ditto,
+            coord_addr: None,
+            coord_doc_id: None,
+            coord_info: None,
+            transport_config: None,
+            hb_doc_id: None,
+            hb_ctx: None,
+            hb_thread: None,
+            start_time_msec: system_time_msec(),
+            local_ip: local_ip.to_string(),
+            state: Arc::new(Mutex::new(PeerState::Init)),
+            peer_collection: None,
+            peer_observer: None,
+        }
+    }
+
+    pub fn state_transition(
+        &mut self,
+        existing: Option<PeerState>,
+        new: PeerState,
+    ) -> Result<(), Box<dyn Error>> {
+        let mut state = self.state.lock().unwrap();
+        assert!(existing.is_none() || existing.unwrap() == *state);
+        println!("--> state_transition: {:?} -> {:?}", self.state, new);
+        *state = new;
+        Ok(())
+    }
+}
