97c9470448f9956026fe82e0a57a37f69054bec5
Aaron Fabbri <ajfabbri@gmail.com>
2023-11-21 19:25:42 -0800

    docker: update generate-compose.sh

--- docker/generate-compose.sh
+++ docker/generate-compose.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 scale=$1
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+source .env
 
 cat <<'EOT'
 version: '2'
@@ -10,49 +11,53 @@ version: '2'
 services:
     coordinator:
         container_name: coordinator
+        env_file:
+          - .env
+          - .secret.env
         build:
             context: .
             dockerfile: ./docker/Dockerfile.coord
             args:
                 FLAVOR:
+EOT
+    cat <<'EOT'
+                ARCH: ${ARCH}
+EOT
+    cat <<EOT
                 BIND_ADDR: 10.1.0.2
                 BIND_PORT: 4001
-                DITTO_APP_ID: "${DITTO_APP_ID}"
-                DITTO_PG_TOKEN: "${DITTO_PG_TOKEN}"
-                DITTO_LICENSE: "${DITTO_LICENSE}"
 
         expose: ["4000-4099"]
-#       ports:
-#          - "4000-4099:4000-4099"
         networks:
           mesh:
             ipv4_address: 10.1.0.2
 EOT
 
-for (( i = 0; i<scale; i++)); do
+for (( i = 1; i<=scale; i++)); do
     block_sz=10
     beginport=$((5100 + ( i * block_sz) ))
     endport=$((beginport + block_sz - 1))
     cat <<EOT
+
     peer$i:
         container_name: peer$i
+        env_file:
+          - .env
+          - .secret.env
         build:
             context: .
             dockerfile: ./docker/Dockerfile.peer
             args:
-                DEVICE_NAME: "peer$i"
                 FLAVOR:
-                BIND_PORT: $beginport
-                COORD_ADDR: 10.1.0.2
-                COORD_PORT: 4001
 EOT
-
     cat <<'EOT'
-                DITTO_APP_ID: "${DITTO_APP_ID}"
-                DITTO_PG_TOKEN: "${DITTO_PG_TOKEN}"
-                DITTO_LICENSE: "${DITTO_LICENSE}"
+                ARCH: ${ARCH}
 EOT
     cat <<EOT
+                COORD_ADDR: 10.1.0.2
+                COORD_PORT: 4001
+                BIND_PORT: $beginport
+                DEVICE_NAME: peer$i
         expose: ["$beginport-$endport"]
         networks:
           - mesh
@@ -60,6 +65,7 @@ EOT
 done
 
 cat <<EOT
+
 networks:
     mesh:
         driver: bridge
