52b3df8e4becf4f47626d683924f90ad902af9eb
Aaron Fabbri <aaron.fabbri@ditto.live>
2023-12-23 16:18:40 -0800

    cmesh/ts: unit test stuff and skeleton test.

--- container-mesh/ts/src/cmditto.test.ts
+++ container-mesh/ts/src/cmditto.test.ts
@@ -1,28 +1,29 @@
 import { CmeshDitto, CmeshEvent } from './cmditto';
 
-// main function
-async function main() {
-    let cmditto = new CmeshDitto()
-    await cmditto.start(async (event: CmeshEvent) => {
+test('cmditto app lifecycle', async () => {
+    let begin = false
+    let end = false
+    let exit = false
+    let cditto = new CmeshDitto()
+    let fut = cditto.start(async (event: CmeshEvent) => {
         switch (event) {
             case CmeshEvent.BeginTest:
                 console.log("BeginTest")
+                begin = true
                 break
             case CmeshEvent.EndTest:
                 console.log("EndTest")
+                end = true
                 break
             case CmeshEvent.Exiting:
                 console.log("Exiting")
+                exit = true
                 break
             default:
                 // fail assertion in this case
                 fail("Unexpected event")
-            }
-        })
-}
-
-try {
-    main()
-} catch (e) {
-    console.log(e);
-}
+        }
+    })
+    await fut
+    expect(begin && end && exit).toBe(true)
+})
--- container-mesh/ts/src/cmditto.ts
+++ container-mesh/ts/src/cmditto.ts
@@ -1,11 +1,31 @@
 import { Ditto } from '@dittolive/ditto';
 
+export enum CmeshEvent {
+    BeginTest,
+    EndTest,
+    Exiting
+};
+
+type CMEventCallback = (event: CmeshEvent) => Promise<void>;
 export class CmeshDitto {
-    ditto: Ditto;
+    //ditto: Ditto;
     hello(who: string): void {
-        console.log(`Hello ${who}!`);
+        console.log(`Hello ${who}!`)
     }
     constructor() {
-        this.ditto = new Ditto();
+        //this.ditto = new Ditto()
     }
+
+    // Call start() to get an event callback for state transitions.
+    public async start(cb: CMEventCallback) : Promise<string> {
+        // XXX
+        cb(CmeshEvent.BeginTest)
+        // sleep for 1 second
+        await new Promise(resolve => setTimeout(resolve, 100));
+        await cb(CmeshEvent.EndTest)
+        await new Promise(resolve => setTimeout(resolve, 100));
+        await cb(CmeshEvent.Exiting)
+        return Promise.resolve("OK")
+    }
+
 }
